695434e497438e032921bce38bb7808235ec8bc5
AntonJRoscher <antonjohanro@gmail.com>
2019-09-12 00:25:33 +0200

    Updated Login And Master Page
    <href = " Readme.mv" > PLease read this for any uncertainties (Andrew).
    
    Base Page to work off :)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <!-- MasterPage -->
   <div id="app">
-    <div class = "parent">
+    <div class="parent">
       <section>
         <!-- Navigation Bar -->
         <b-navbar>
           <template slot="brand">
-            <b-navbar-item class = "itemHeight" href="/" >
-              <img
-                      src="../src/assets/images/Logo.png"
-              >
+            <b-navbar-item class="itemHeight" href="/">
+              <img src="../src/assets/images/Logo.png" />
             </b-navbar-item>
           </template>
           <template slot="start">
@@ -28,7 +26,6 @@
             <b-navbar-item>
               Find The Perfect Bean Type
             </b-navbar-item>
-
           </template>
 
           <template slot="end">
@@ -36,109 +33,36 @@
               <strong>Cart</strong>
             </b-navbar-item>
 
-
-      <b-navbar-dropdown label="Account">
-            <div class="navbar-menu">
-              <div class="navbar-end">
-                <b-dropdown position="is-bottom-left" aria-role="menu">
-                  <a
-                          class="navbar-item"
-                          slot="trigger"
-                          role="button">
-                    <span>Login</span>
-                    <b-icon icon="menu-down"></b-icon>
-                  </a>
-
-                  <b-dropdown-item
-                          aria-role="menu-item"
-                          :focusable="false"
-                          custom
-                          paddingless>
-                    <form action="">
-                      <div class="modal-card" style="width:300px;">
-                        <section class="modal-card-body">
-                          <b-field label="Email">
-                            <b-input
-                                    type="email"
-                                    placeholder="Your email"
-                                    required>
-                            </b-input>
-                          </b-field>
-
-                          <b-field label="Password">
-                            <b-input
-                                    type="password"
-                                    password-reveal
-                                    placeholder="Your password"
-                                    required>
-                            </b-input>
-                          </b-field>
-
-                          <b-checkbox>Remember me</b-checkbox>
-                        </section>
-                        <footer class="modal-card-foot">
-                          <button class="button is-primary">Login</button>
-                        </footer>
-                      </div>
-                    </form>
-                  </b-dropdown-item>
-
-                </b-dropdown>
-              </div>
-            </div>
-      </b-navbar-dropdown>
-
-
-
-
-
-            <b-navbar-dropdown>
-              <strong> Account </strong>
-              <div>
-            <b-navbar-item tag="div">
-              <div class="buttons">
-                <a class="button is-primary">
-                  <strong>Sign up</strong>
-                </a>
-                <a class="button is-light">
-                  Log in
-                </a>
-              </div>
+            <b-navbar-item>
+              <LoginNav>Accounts</LoginNav>
             </b-navbar-item>
-              </div>
-            </b-navbar-dropdown>
-
 
           </template>
-
         </b-navbar>
-
       </section>
     </div>
     <!--   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <div>
-
-    </div>
-    <footer>
-
-    </footer>
+    <div></div>
+    <footer></footer>
   </div>
 </template>
 
+
+
 <script>
-
-  export default {
-    name: 'app',
-    components: {
-
-      // Store Homepage as var Homepage --> Do not save as just Homepage
-
-    }
+  //Import for Login
+  import  LoginNav from './components/LoginNav'
+export default {
+  name: "app",
+  components: {
+    LoginNav
+    // Store Homepage as var Homepage --> Do not save as just Homepage
   }
+};
 </script>
 
 <style lang="scss" scoped>
-  /*#app {
+/*#app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
@@ -146,36 +70,35 @@
     color: #2c3e50;
     margin-top: 60px;
   } */
-  /* Resize items of navbar */
-  .itemHeight{
-    height: 100px;
+/* Resize items of navbar */
+.itemHeight {
+  height: 100px;
+}
+/*Resize just the img element of Navbar */
+.navbar-item img {
+  max-height: 100px;
 
-  }
-  /*Resize just the img element of Navbar */
-  .navbar-item img{
-    max-height: 100px;
-  }
+}
 
-  .navbar {
-    background-color: #BCB8B9;
-    min-height: 100px ;
-    position: relative;
-    z-index: 30;
-  }
+.navbar {
+  background-color: #bcb8b9;
+  min-height: 100px;
+  position: relative;
+  z-index: 30;
+}
 
-  .button.is-primary {
-    background-color: #00ADDA;
-    border-color: transparent;
-    color: white;
-  }
+.button.is-primary {
+  background-color: #00adda;
+  border-color: transparent;
+  color: white;
+}
 
-  .box.is-primary{
-    background-color: #00ADDA;
-  }
+.box.is-primary {
+  background-color: #00adda;
+}
 
-  .hero.is-link {
-    background-color : #181a1b  ;
-  }
-
+.hero.is-link {
+  background-color: #181a1b;
+}
 
 </style>
---
+++ src/components/LoginNav.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-dropdown position="is-bottom-left" aria-role="menu">
+    <a class="navbar-item" slot="trigger" role="button">
+      <span>Accounts</span>
+      <b-icon icon="menu-down"></b-icon>
+    </a>
+
+    <b-dropdown-item
+      aria-role="menu-item"
+      :focusable="false"
+      custom
+      paddingless
+    >
+      <form action="">
+        <div class="modal-card" style="width:300px;">
+          <section class="modal-card-body">
+            <b-field label="Email">
+              <b-input type="email" placeholder="Your email" required>
+              </b-input>
+            </b-field>
+
+            <b-field label="Password">
+              <b-input
+                type="password"
+                password-reveal
+                placeholder="Your password"
+                required
+              >
+              </b-input>
+            </b-field>
+
+            <b-checkbox>Remember me</b-checkbox>
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button is-primary">Login</button>
+<!--              TODO Register page   -->
+            <button class="button" href="">Register</button>
+          </footer>
+        </div>
+      </form>
+    </b-dropdown-item>
+  </b-dropdown>
+</template>
