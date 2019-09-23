e467f9b4c08cc2b286f9b5e415bafd6a3e940d06
Thomas McAlpine <thomasmcalpine77@gmail.com>
2019-09-23 19:57:39 +0200

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,66 @@
 <template>
-  <section>
-    <div aria-role="menu-item" :focusable="false" custom paddingless>
-      <form action="">
-        <div class="modal-card" style="width:300px;">
-          <section class="modal-card-body">
-            <b-field label="Email">
-              <b-input type="email" placeholder="Your email" required>
-              </b-input>
-            </b-field>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-3 ">
+        <h1 class="title has-text-centered">Login</h1>
 
-            <b-field label="Password">
-              <b-input
-                type="password"
-                password-reveal
-                placeholder="Your password"
-                required
-              >
-              </b-input>
-            </b-field>
+        <div class="box">
+          <form @submit="formSubmit">
+            <!-- USERNAME -->
+            <div class="field">
+              <label class="label">Username</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="Enter username" />
+              </div>
+            </div>
+            <!-- //USERNAME -->
 
-            <b-checkbox>Remember me</b-checkbox>
-          </section>
-          <footer class="modal-card-foot">
-            <button class="button is-primary">Login</button>
-          </footer>
+            <!-- PASSWORD -->
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input class="input" type="password" placeholder="Password" />
+              </div>
+            </div>
+            <!-- //PASSWORD -->
+
+            <button class="button is-fullwidth is-info">Login</button>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+import axios from "axios";
 export default {
-  name: "Login"
+  name: "Login",
+  mounted() {
+    axios.get("http://localhost:5000/api/values").then(function(response) {
+      // eslint-disable-next-line no-console
+      console.log(response.data);
+    });
+  },
+  methods: {
+    formSubmit(e) {
+      e.preventDefault();
+      axios
+        .post("http://localhost:5000/api/Login", {
+          username: this.username,
+          password: this.password
+        })
+        .then(function(response) {
+          // eslint-disable-next-line no-console
+         console.log(response);
+        })
+        .catch(function(error) {
+          // eslint-disable-next-line no-console
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
 
