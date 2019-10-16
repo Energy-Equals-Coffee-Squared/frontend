8e14772a62d91e672c7de5566c604e40d5ea617a
AntonJRoscher <antonjohanro@gmail.com>
2019-10-16 07:03:07 +0200

    FINISHED ! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,22 +6,23 @@
       <div class="row">
         <!--      For out images as a team -->
         <div class="columns">
-          <div class="column is-rounded">
-            <img src="../assets/images/Avatars.png" />
-            <p class="tile" style="position:center">Grey Hat #1</p>
-            <p class="subtitle" style=" position: center">Andrew Walls</p>
-          </div>
 
           <div class="column is-rounded">
             <img src="../assets/images/Avatars.png" />
-            <p class="tile" style="position:center">Grey Hat #2</p>
+            <p class="tile" style="position:center">Grey Hat #1</p>
             <p class="subtitle" style=" position: center">Thomas McAlpine</p>
           </div>
 
           <div class="column is-rounded">
             <img src="../assets/images/Avatars.png" />
-            <p class="tile" style="position:center">Grey Hat #3</p>
+            <p class="tile" style="position:center">Grey Hat #2</p>
             <p class="subtitle" style=" position: center">Anton Röscher</p>
+          </div>
+
+          <div class="column is-rounded">
+            <img src="../assets/images/Avatars.png" />
+            <p class="tile" style="position:center">Grey Hat #3</p>
+            <p class="subtitle" style=" position: center">Andrew Walls</p>
           </div>
         </div>
       </div>
