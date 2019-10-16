60e2d3e3bb29dbea2800f485f7b73ed930f2894f
AntonJRoscher <antonjohanro@gmail.com>
2019-10-16 05:12:54 +0200

    Update Reports.vue

--- src/views/Reports.vue
+++ src/views/Reports.vue
@@ -1,13 +1,56 @@
 <template>
-    <div></div>
+  <div class="section" align="center">
+    <div class="box">
+      <h1 class="is-size-1"><strong> Reports </strong></h1>
+
+      <div class="columns is-multiline">
+        <div class="column is-4">
+          <div class="box">
+            <h1>Users Summary</h1>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="box">
+            <h1>Orders Summary</h1>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="box">
+            <h1>Number Products</h1>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="box">
+            <h1>Number Products Sold</h1>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="box">
+            <h1>Most Sold Product</h1>
+          </div>
+        </div>
+
+          <div class="column is-4">
+              <div class="box">
+                  <h1>Total Amount Made</h1>
+              </div>
+          </div>
+
+
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Reports"
-    }
+export default {
+  name: "Reports"
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
