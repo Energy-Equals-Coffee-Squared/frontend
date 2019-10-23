7e084273d33824e71ba9551cdac32b590743da4f
AntonJRoscher <antonjohanro@gmail.com>
2019-10-23 05:38:26 +0200

    Homepage
    Fixed Homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,25 @@
 <template>
     <div>
-        <div class="col" align="center" style="margin-top: 25px">
-        <h1 class="is-size-1 has-text-grey-lighter">  Need Beans ?  </h1>
-        </div>
- <div class="container" style="transform: scale(); margin-top: 50px">
-   <carousel>
-     <slide class="slide"> <img src="../../src/assets/images/home/1.jpg" height="672" width="477.22">
-     </slide>
-       <slide class="slide"> <img src="../../src/assets/images/home/2.jpg" height="672" width="477.22">
-       </slide>
-       <slide class="slide"> <img src="../../src/assets/images/home/3.jpg" height="672" width="477.22">
-       </slide>
+<!--        <div class="col" align="center" style="margin-top: 25px">-->
+<!--        <h1 class="is-size-1 has-text-grey-lighter">  Need Beans ?  </h1>-->
+<!--        </div>-->
 
-           <slide class="slide"> <img src="../../src/assets/images/home/4.jpg" height="672" width="477.22">
-           </slide>
-   </carousel>
- </div>
+
+
+
+            <img style=" opacity:0.65" src="../../src/assets/images/Wall.jpg" height="1920" width="2000" />
+
         <div class="col" align="center">
-        <h1 class="is-size-1 has-text-grey-lighter"> We Got You !  </h1>
+            <img style="position:absolute; top:130px; left:480px" src="../../src/assets/images/Circle.png" height="500" width="550">
         </div>
+
+        <div class="col" align="center">
+            <img style="position:absolute; top:160px; left:530px" src="../../src/assets/images/Logo.png" height="500" width="500">
+        </div>
+
+
+        <h1 style="position:absolute; top:350px; left:70px" class="is-size-1 has-text-grey-lighter">Need beans?  </h1>
+        <h1 style="position:absolute; top:350px; right:80px" class="is-size-1 has-text-grey-lighter"> We Got You!  </h1>
+        <h1 style="position:absolute; top:690px; left:500px"  class="is-size-0.2 has-text-grey-lighter"> “Coffee, the finest organic suspension ever devised.” – Captain Janeway  </h1>
+
   </div>
-</template>
-
-<script>
-  import { Carousel, Slide } from 'vue-carousel';
-
-export default {
-  components: {
-    Carousel,
-    Slide
-  }
-};
-</script>
-
-<style>
-    .VueCarousel-wrapper{
-        height: 500px;
-    }
-    .VueCarousel-slide{
-        flex-basis:auto;
-        /*margin-left: 10px;*/
-        /*margin-right: 10px;*/
-    }
-
-</style>
