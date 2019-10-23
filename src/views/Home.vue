8f3b6c5d963dd44340bb1b898293ccc4073b5735
AntonJRoscher <antonjohanro@gmail.com>
2019-10-23 05:59:54 +0200

    Update Home.vue
    Fixed Styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,16 +10,16 @@
             <img style=" opacity:0.65" src="../../src/assets/images/Wall.jpg" height="1920" width="2000" />
 
         <div class="col" align="center">
-            <img style="position:absolute; top:130px; left:480px" src="../../src/assets/images/Circle.png" height="500" width="550">
+            <img style="position:absolute; top:130px; left:680px" src="../../src/assets/images/Circle.png" height="500" width="550">
         </div>
 
         <div class="col" align="center">
-            <img style="position:absolute; top:160px; left:530px" src="../../src/assets/images/Logo.png" height="500" width="500">
+            <img style="position:absolute; top:160px; left:730px" src="../../src/assets/images/Logo.png" height="500" width="500">
         </div>
 
 
-        <h1 style="position:absolute; top:350px; left:70px" class="is-size-1 has-text-grey-lighter">Need beans?  </h1>
-        <h1 style="position:absolute; top:350px; right:80px" class="is-size-1 has-text-grey-lighter"> We Got You!  </h1>
-        <h1 style="position:absolute; top:690px; left:500px"  class="is-size-0.2 has-text-grey-lighter"> “Coffee, the finest organic suspension ever devised.” – Captain Janeway  </h1>
+        <strong><h1  class="is-size-1 has-text-grey-lighter" style="position:absolute; top:350px; left:170px" >Need beans?  </h1></strong>
+        <h1 style="position:absolute; top:350px; right:120px" class="is-size-1 has-text-grey-lighter"> We Got You!  </h1>
+        <h1 style="position:absolute; top:690px; left:590px"  class="is-size-4 has-text-grey-lighter"> “Coffee, the finest organic suspension ever devised.” – Captain Janeway  </h1>
 
   </div>
